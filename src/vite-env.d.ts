b98919a22270876b7c0b00c72cfa568219e8ff22
Craig Analytics <ejemegwapeter@gmail.com>
2025-10-05 12:12:46 -0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,3 @@
 /// <reference types="vite/client" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/log-hub-marketplace/', // 👈 this is VERY important
-})
 
